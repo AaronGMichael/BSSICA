098d0f0c75d9e42e8e43be68d72285dcf8fcbd4a
AaronGMichael <aaron.g.michael@gmail.com>
2022-03-17 17:58:08 +0530

    New Correlation with Spectrum and Envelopes

--- corrplot.py
+++ corrplot.py
@@ -1,20 +1,25 @@
 import sklearn
 import numpy as np
 import pandas as pd
+from pydub import AudioSegment
+import soundfile as sf
 from scipy import signal
 from scipy.io import wavfile
 from two_channelip import mix_sources
 from matplotlib import pyplot as plt
 from numpy import sin, linspace, pi
 from pylab import plot, show, title, xlabel, ylabel, subplot
-from scipy import fft, arange
+from numpy import arange
 import seaborn as sns
-from pydub import AudioSegment
 
 
 def corrplot():
-    sampling_rate, SOURCE_FILE = wavfile.read('Resources/StrTru1stringString.wav')
-    sampling_rate, TARGET_FILE = wavfile.read('Resources/Outputs/split2sig4.wav')
+    #sampling_rate, SOURCE_FILE = wavfile.read('Resources/source2.wav')
+    # SOURCE_FILE = AudioSegment.from_file(file='Resources/source2.wav', format='wav')
+    #sampling_rate2, TARGET_FILE = wavfile.read('Resources/Outputs/out2.wav')
+    # TARGET_FILE = AudioSegment.from_file(file='Resources/Outputs/out2.wav', format='wav')
+    SOURCE_FILE, sampling_rate = sf.read("Resources/StrTruLapBassplucks.wav")
+    TARGET_FILE, sampling_rate2 = sf.read("Resources/Outputs/split2sig2.wav")
     y = pd.Series(SOURCE_FILE)
     x = pd.Series(TARGET_FILE)
     correlation = y.corr(x)
@@ -32,30 +37,30 @@ def corrplot():
     X = mix_sources([SOURCE_FILE, TARGET_FILE])
     fig = plt.figure()
     plt.subplot(4, 1, 1)
-    plt.suptitle('String 4 Source Correlation')
+    plt.suptitle('Plucks(guitar) Correlation for String+Trumpet+Bass+Plucks Mix')
     plt.scatter(x, y)
     plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), color='red')
     plt.xlabel('x axis')
     plt.ylabel('y axis')
     plt.title("correlation: " + str(correlation))
-    # plt.xlim([4, 6])
-    # plt.ylim([-4e8, 8e8])
     plt.subplot(4, 1, 2)
     for s in X:
-        plt.plot(s)
+        plt.plot(s, alpha=0.8)
     plt.title("Time Domain Envelope")
     plt.subplot(4, 1, 3)
     fft_spectrum = np.fft.rfft(SOURCE_FILE)
-    freq = np.fft.rfftfreq(SOURCE_FILE.size, d=1. / sampling_rate)
+    freq = np.fft.rfftfreq(SOURCE_FILE.size, d=1 / sampling_rate)
     fft_spectrum_abs = np.abs(fft_spectrum)
-    plt.plot(freq, fft_spectrum_abs, color='mediumslateblue', alpha=0.5)
+    plt.plot(freq, fft_spectrum_abs, color='orangered')
+    plt.xlim([0, 5000])
     plt.xlabel("frequency, Hz")
     plt.ylabel("Amplitude, units")
     plt.subplot(4, 1, 4)
-    fft_spectrum= np.fft.rfft(TARGET_FILE)
-    freq = np.fft.rfftfreq(TARGET_FILE.size, d=1. / sampling_rate)
+    fft_spectrum = np.fft.rfft(TARGET_FILE)
+    freq = np.fft.rfftfreq(TARGET_FILE.size, d=1. / sampling_rate2)
     fft_spectrum_abs = np.abs(fft_spectrum)
-    plt.plot(freq, fft_spectrum_abs, color='orangered', alpha=0.5)
+    plt.plot(freq, fft_spectrum_abs, color='orangered',)
+    plt.xlim([0, 5000])
     plt.xlabel("frequency, Hz")
     plt.ylabel("Amplitude, units")
     fig.tight_layout()
